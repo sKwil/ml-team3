a2bd0c2994d94ad1ad2fd50095240319592bf3e7
threehalves-pi <1.5pi.statshelper@gmail.com>
2022-03-18 23:05:07 -0400

    Format database setup script

--- model/dataCleaning/setup.sql
+++ model/dataCleaning/setup.sql
@@ -1,22 +1,24 @@
 DROP TABLE IF EXISTS Cities;
 DROP TABLE IF EXISTS Weather;
 
-CREATE TABLE Cities (
-    name TEXT PRIMARY KEY,
-    country TEXT,
-    latitude REAL,
+CREATE TABLE Cities
+(
+    name      TEXT PRIMARY KEY,
+    country   TEXT,
+    latitude  REAL,
     longitude REAL
 );
 
-CREATE TABLE Weather (
-    datetime TIMESTAMP,
-    city TEXT NOT NULL,
-    temperature REAL,
-    humidity REAL,
-    pressure REAL,
+CREATE TABLE Weather
+(
+    datetime            TIMESTAMP,
+    city                TEXT NOT NULL,
+    temperature         REAL,
+    humidity            REAL,
+    pressure            REAL,
     weather_description TEXT,
-    wind_direction REAL,
-    wind_speed REAL,
+    wind_direction      REAL,
+    wind_speed          REAL,
     PRIMARY KEY (datetime, city),
     FOREIGN KEY (city) REFERENCES Cities (name)
 );
