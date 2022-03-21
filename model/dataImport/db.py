8d68376b816a04ba98a6bb8c7853ecbdb27a51e7
AGrunt <trenton.metcalfe@eastern.edu>
2022-03-21 14:52:05 -0400

    Add sqlite to dataframe method

---
+++ model/dataImport/db.py
@@ -0,0 +1,36 @@
+import os
+import sqlite3
+import pandas as pd
+
+WORKING_DIR = os.getcwd()
+
+DATA_DIR = os.path.join(
+    os.path.abspath(os.path.join(WORKING_DIR, os.pardir, os.pardir)),
+    'data'
+)
+
+SQL_DIR = os.path.join(DATA_DIR, 'sql')
+
+DATABASE = os.path.join(SQL_DIR, 'weather.db')
+
+
+def getConn() -> sqlite3.Connection:
+    """
+    Get a connection to the sqlite database where all the weather data is
+    stored.
+
+    :return: a sqlite connection
+    """
+
+    return sqlite3.connect(DATABASE)
+
+
+def getDataFrame():
+    """
+    convert the weather sqlite table into pandas data frame
+    """
+
+    with getConn() as conn:
+        df = pd.read_sql_query("SELECT * FROM Weather", conn)
+
+    return df
---
+++ model/dataImport/test.py
@@ -0,0 +1,5 @@
+import db
+
+df = db.getDataFrame()
+
+print(df.tail())
