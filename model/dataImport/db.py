90b31b0ec466f65a9513e6360e9cf699c7236a9d
AGrunt <trenton.metcalfe@eastern.edu>
2022-03-22 09:52:15 -0400

    Change the dataimport db file such that it reads from the USWeather View.

--- model/dataImport/db.py
+++ model/dataImport/db.py
@@ -30,7 +30,8 @@ def getDataFrame():
     convert the weather sqlite table into pandas data frame
     """
 
+    # Get the connection to the table of interest. Currently set as the USWeather view.
     with getConn() as conn:
-        df = pd.read_sql_query("SELECT * FROM Weather", conn)
+        df = pd.read_sql_query("SELECT * FROM USWeather", conn)
 
     return df
