831e35e737ace37736ba6d108a6179fd0fa3f78e
threehalves-pi <1.5pi.statshelper@gmail.com>
2022-03-22 22:53:05 -0400

    Add page for each group member

--- website/__init__.py
+++ website/__init__.py
@@ -14,5 +14,4 @@ def create_app():
     from . import user
     app.register_blueprint(user.bp)
 
-
     return app
--- website/user.py
+++ website/user.py
@@ -6,3 +6,20 @@ bp = Blueprint('user', __name__)
 @bp.route('/')
 def index():
     return render_template('user/simon.html')
+
+@bp.route('/simon')
+def index():
+    return render_template('user/simon.html')
+
+@bp.route('/trenton')
+def index():
+    return render_template('user/trenton.html')
+
+@bp.route('/sean')
+def index():
+    return render_template('user/sean.html')
+
+@bp.route('/jermaine')
+def index():
+    return render_template('user/jermaine.html')
+
