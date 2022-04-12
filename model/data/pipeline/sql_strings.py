f56f4640736582d2f4ab4316d57bd2d143140287
sKwil <simon.kwilinski@eastern.edu>
2022-04-12 09:03:08 -0400

    Remove unused resources file

--- model/data/pipeline/sql_strings.py
+++ model/data/pipeline/sql_strings.py
@@ -3,6 +3,4 @@ These SQL strings are used during the pipeline process, primarily by the
 aggregation file.
 """
 
-from model.data.pipeline import resources as re
-
 GET_MONTHLY_DATA = 'SELECT * From MonthlyData;'
