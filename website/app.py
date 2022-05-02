eb3d2f3903967b9114784246fda49988f9adc887
Jermaine <bigpres56@hotmail.com>
2022-05-02 14:59:32 -0400

    adding front end intereface

---
+++ website/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, escape, request, render_template
+import pandas as pd
+import joblib
+
+
+def preprocessDataAndPredict(state, curr_time, curr_occ, occ_24, occ_week, occ_month):
+    # keep all inputs in array
+    data = [curr_day, curr_time, curr_occ, occ_24, occ_week, occ_month]
+
+    # Create Data Frame
+    data = pd.DataFrame({'day': [curr_day], 'curr_time': [curr_time],
+         'curr_occ': [curr_occ], 'occ_24': [occ_24], 'occ_week': [occ_week],
+         'occ_month': [occ_month]})
+
+    # open file
+    file = open("finalModel.pkl", "rb")
